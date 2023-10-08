7a178e2f2e2473d7d37f343dddbb0870520d9a74
prezeswastaken <kacper.preyzner@studenci.collegiumwitelona.pl>
2023-10-08 14:10:32 +0200

    consuming hello message from api

--- frontend-wheel-wallet/app.vue
+++ frontend-wheel-wallet/app.vue
@@ -1,6 +1,16 @@
+<script setup lang="ts">
+const messageFromApi = ref("Waiting for response from API...");
+async function fetchHello() {
+  const response = await fetch("//localhost/api/hello");
+  const data = await response.json();
+  messageFromApi.value = data.message;
+  console.log(data.message);
+}
+onMounted(() => {
+  fetchHello();
+});
+</script>
 <template>
-  <div>
-    Hello from docker!;
-    <NuxtWelcome />
-  </div>
+  <div>Hello from docker!</div>
+  <div>Message from API: {{ messageFromApi }}</div>
 </template>
