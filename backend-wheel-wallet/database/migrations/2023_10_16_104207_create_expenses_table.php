60f857c834610faba0d7b5ec9db8999b0f31842e
Paweł Orłowski <orlowski11@o2.pl>
2023-10-16 19:42:30 +0200

    added migrations

--- backend-wheel-wallet/database/migrations/2023_10_16_104207_create_expenses_table.php
+++ backend-wheel-wallet/database/migrations/2023_10_16_104207_create_expenses_table.php
@@ -11,15 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('expenses', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained(table: 'users')->onDelete('cascade');
+            $table->foreignId('car_id')->constrained(table: 'cars')->onDelete('cascade');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->binary('profile_picture')->nullable();
-            $table->boolean('role')->default('false');
-            $table->rememberToken();
+            $table->bigInteger('cost')->nullable();
+            $table->date('date');
+            $table->boolean('planned');
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('expenses');
     }
 };
