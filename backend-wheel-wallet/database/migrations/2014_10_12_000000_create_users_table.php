4052890bbe20c146b27ff514c25b4c57c3756d26
prezeswastaken <kacper.preyzner@studenci.collegiumwitelona.pl>
2023-10-18 17:45:05 +0200

    changed 'role' field in users table to 'is_admin'

--- backend-wheel-wallet/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend-wheel-wallet/database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -18,7 +17,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->binary('profile_picture')->nullable();
-            $table->boolean('role')->default('false');
+            $table->boolean('is_admin')->default('false');
             $table->rememberToken();
             $table->timestamps();
         });
