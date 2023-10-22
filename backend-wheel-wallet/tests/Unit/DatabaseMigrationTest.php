02acad9b22b890f45f46ee989bc684dd022b3397
DominikSk0rka <Sk0rka.dev@gmail.com>
2023-10-22 12:23:40 +0200

    Update migration test (backend)

--- backend-wheel-wallet/tests/Unit/DatabaseMigrationTest.php
+++ backend-wheel-wallet/tests/Unit/DatabaseMigrationTest.php
@@ -10,24 +10,23 @@ class DatabaseMigrationTest extends TestCase
 {
     use RefreshDatabase;
 
-    /** @test */
+   
     public function test_users_table_has_expected_columns()
     {
-        // Uruchom migracje
+       
         $this->artisan('migrate');
 
-        // Sprawdź, czy tabela "users" istnieje w bazie danych
         $this->assertTrue(Schema::hasTable('users'));
 
-        // Sprawdź, czy tabela zawiera oczekiwane kolumny
+      
         $this->assertTrue(Schema::hasColumns('users', [
             'id',
             'name',
             'email',
             'password',
-            // Dodaj inne kolumny, które powinny istnieć w tabeli
+          
         ]));
     }
 
-    // Tutaj możesz dodać inne testy dotyczące migracji, np. dodawanie innych tabel, indeksów itp.
+   
 }
