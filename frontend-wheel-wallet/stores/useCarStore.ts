4aa14f12fce535673225849f08b0cf33a297ef9f
prezeswastaken <kacper.preyzner@studenci.collegiumwitelona.pl>
2023-10-24 21:21:47 +0200

    creating cars works

--- frontend-wheel-wallet/stores/useCarStore.ts
+++ frontend-wheel-wallet/stores/useCarStore.ts
@@ -2,12 +2,29 @@ import { defineStore } from "pinia";
 
 import { Car } from "~/types/CarType";
 
+type CarCreateData = {
+  model: string;
+  status: string;
+  owner_id: number;
+};
+
 export const useCarStore = defineStore("car", () => {
   const cars = ref<Car[] | null>(null);
 
   async function fetchCarsByUserID(userID: number) {
     const { data, error } = await useApiFetch(`/api/user/${userID}/cars`);
     cars.value = data.value as Car[];
+  }
+
+  async function createCar(carCreateData: CarCreateData) {
+    if (carCreateData.status == "") {
+      carCreateData.status = "(empty)";
+    }
+    const response = await useApiFetch("/api/car", {
+      method: "post",
+      body: carCreateData,
+    });
+    return response;
   }
 
   const hasCars = computed(() => {
@@ -18,5 +35,5 @@ export const useCarStore = defineStore("car", () => {
     }
   });
 
-  return { fetchCarsByUserID, cars, hasCars };
+  return { fetchCarsByUserID, cars, hasCars, createCar };
 });
